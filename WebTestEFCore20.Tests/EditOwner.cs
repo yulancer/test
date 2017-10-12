745b5bbea7a42035f1e5473e8f2eb4228cd19075
matveev_yuri <yulancer@yandex.ru>
2017-10-12 16:44:14 +0300

    тест контроллера готов

--- WebTestEFCore20.Tests/EditOwner.cs
+++ WebTestEFCore20.Tests/EditOwner.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using WebTestEFCore20.Controllers;
+using WebTestEFCore20.Models;
 
 namespace WebTestEFCore20.Tests
 {
@@ -9,16 +12,46 @@ namespace WebTestEFCore20.Tests
         [Test]
         public void Register_GetView_ItsOkViewModelIsUserView()
         {
+            AccountOwner owner7876Old;
+            AccountOwner owner5678Old;
+            using (TestDbContext context = new TestDbContext())
+            {
+                Account acc5678 = context.Accounts
+                    .Include(a => a.Owner).FirstOrDefault(a => a.Number == 5678);
+                Account acc7876 = context.Accounts
+                    .Include(a => a.Owner).FirstOrDefault(a => a.Number == 7876);
 
-            var a = 1;
-            Assert.AreEqual(a, 1);
-            //Console.WriteLine("=====INIT======");
+                Assert.NotNull(acc7876, "Не найден счет с номером 7876");
+                Assert.NotNull(acc5678, "Не найден счет с номером 5678");
+
+                owner7876Old = acc7876.Owner;
+                owner5678Old = acc5678.Owner;
+
+                Assert.NotNull(owner7876Old, "Для счета с номером 7876 не указан владелец");
+                Assert.NotNull(owner5678Old, "Для счета с номером 5678 не указан владелец");
+            }
+
             var controller = new HomeController();
-            //Console.WriteLine("======ACT======");
             var result = controller.EditOwner();
-            //Console.WriteLine("====ASSERT=====");
-            //Assert.IsInstanceOf<ViewResult>(result);
-            //Assert.IsInstanceOf<IList<AccountViewModel>>(((ViewResult)result).Model);
+
+            using (TestDbContext context = new TestDbContext())
+            {
+                Account acc5678 = context.Accounts
+                    .Include(a => a.Owner).FirstOrDefault(a => a.Number == 5678);
+                Account acc7876 = context.Accounts
+                    .Include(a => a.Owner).FirstOrDefault(a => a.Number == 7876);
+
+                AccountOwner owner7876New = acc7876.Owner;
+                AccountOwner owner5678New = acc5678.Owner;
+
+                Assert.NotNull(owner7876New, "У счета с номером 7876 потерялся владелец");
+                Assert.NotNull(owner5678New, "У счета с номером 5678 потерялся владелец");
+
+                Assert.AreEqual(owner7876Old.Id, owner5678New.Id
+                    , "У счета с номером 5678 владелец поменялся неправильно");
+                Assert.AreEqual(owner5678Old.Id, owner7876New.Id
+                    , "У счета с номером 7876 владелец поменялся неправильно");
+            }
         }
     }
 }
