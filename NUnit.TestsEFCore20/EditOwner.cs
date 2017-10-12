ca1c258156a864d7b27904683ebe1b1dc7904d5c
matveev_yuri <yulancer@yandex.ru>
2017-10-12 16:21:58 +0300

    заработали тесты

---
+++ NUnit.TestsEFCore20/EditOwner.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using WebTestEFCore20;
+
+namespace NUnit.TestsEFCore20
+{
+    [TestFixture]
+    public class EditOwnerControllerTest
+    {
+        [Test]
+        public void Register_GetView_ItsOkViewModelIsUserView()
+        {
+
+            var a = 1;
+            Assert.AreEqual(a, 1);
+            //Console.WriteLine("=====INIT======");
+            //var controller = DbInitializer.
+            //Console.WriteLine("======ACT======");
+            //var result = controller.EditOwner();
+            //Console.WriteLine("====ASSERT=====");
+            //Assert.IsInstanceOf<ViewResult>(result);
+            //Assert.IsInstanceOf<IList<AccountViewModel>>(((ViewResult)result).Model);
+        }
+    }
+}
---
+++ WebTestEFCore20.Tests/EditOwner.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using WebTestEFCore20.Controllers;
+
+namespace WebTestEFCore20.Tests
+{
+    [TestFixture]
+    public class EditOwnerControllerTest
+    {
+        [Test]
+        public void Register_GetView_ItsOkViewModelIsUserView()
+        {
+
+            var a = 1;
+            Assert.AreEqual(a, 1);
+            //Console.WriteLine("=====INIT======");
+            var controller = new HomeController();
+            //Console.WriteLine("======ACT======");
+            var result = controller.EditOwner();
+            //Console.WriteLine("====ASSERT=====");
+            //Assert.IsInstanceOf<ViewResult>(result);
+            //Assert.IsInstanceOf<IList<AccountViewModel>>(((ViewResult)result).Model);
+        }
+    }
+}
